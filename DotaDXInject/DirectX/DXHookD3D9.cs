patriksletmo/Dota2Translator

commit 3d4a956057e4278db3eaf1bf6869344c40a42f1a

Added ability to set an independant font height
Adds the ability to not use an automatic font height based off the
amount of messages shown on the screen. The height can be set in pixels
from settings. Implements suggestion #2.

DotaDXInject/DirectX/DXHookD3D9.cs
@@ -54,6 +54,12 @@ namespace DotaDXInject
         // The last amount of messages shown, used to refresh the font.
         private int LastMessagesShown = 0;
 
+        // The last value of using an automatic message height, used to refresh the font.
+        private Boolean LastAutoMessageHeight = false;
+
+        // The last message height, used to refresh the font.
+        private Int16 LastMessageHeight = 0;
+
         // The last font height, used to refresh the font.
         private int FontHeight = 0;
 
@@ -71,6 +77,9 @@ namespace DotaDXInject
 
         // Y position for the overlay.
         private int TextStartHeight = 0;
+
+        // The bottom Y position of the overlay.
+        private int TextEndHeight = 0;
 
         // The color used to draw the text.
         private Color TextColor = Color.FromArgb(250, 234, 201);
@@ -95,6 +104,12 @@ namespace DotaDXInject
 
         // How long the messages will fade.
         public double FadeDuration = 2.5;
+
+        // Whether or not to use an automatic message height.
+        public Boolean AutoMessageHeight = true;
+
+        // The height of the message font in pixels. Used if AutoMessageHeight is false.
+        public Int16 MessageHeight = 12;
 
         // The time the overlay was added.
         private DateTime TimeAdded;
@@ -314,10 +329,12 @@ namespace DotaDXInject
                     if (!Hide || !AutoHide)
                     {
                         // Refresh the font if the settings has been changed or create a font if null.
-                        if (OverlayFont == null || !FontName.Equals(LastFontName) || LastMessagesShown != MessagesShown)
+                        if (OverlayFont == null || !FontName.Equals(LastFontName) || LastMessagesShown != MessagesShown || AutoMessageHeight != LastAutoMessageHeight || MessageHeight != LastMessageHeight)
                         {
                             LastFontName = FontName;
                             LastMessagesShown = MessagesShown;
+                            LastAutoMessageHeight = AutoMessageHeight;
+                            LastMessageHeight = MessageHeight;
                             LoadFont(device);
                         }
 
@@ -365,18 +382,21 @@ namespace DotaDXInject
                             // Draw the wrapped message from the bottom.
                             for (int j = message.WrappedMessage.Length - 1; j >= 0 && y >= 0; j--)
                             {
-                                if (drawShadow)
-                                    OverlayFont.DrawString(null, message.WrappedMessage[j], TextStartWidth + FontHeight / 2 + message.SenderWidth, TextStartHeight + 1 + FontHeight * y, TextShadowColor);
+                                // Calculate the y position for the string.
+                                int stringY = TextStartHeight + (TextEndHeight - TextStartHeight) / MessagesShown * y; 
 
-                                OverlayFont.DrawString(null, message.WrappedMessage[j], TextStartWidth - 1 + FontHeight / 2 + message.SenderWidth, TextStartHeight + FontHeight * y, drawColor);
+                                if (drawShadow)
+                                    OverlayFont.DrawString(null, message.WrappedMessage[j], TextStartWidth + FontHeight / 2 + message.SenderWidth, stringY + 1, TextShadowColor);
+
+                                OverlayFont.DrawString(null, message.WrappedMessage[j], TextStartWidth - 1 + FontHeight / 2 + message.SenderWidth, stringY, drawColor);
 
                                 // Only draw the sender on the upmost line.
                                 if (j == 0)
                                 {
                                     if (drawShadow)
-                                        OverlayFont.DrawString(null, message.Sender + ": ", TextStartWidth, TextStartHeight + 1 + FontHeight * y, TextShadowColor);
+                                        OverlayFont.DrawString(null, message.Sender + ": ", TextStartWidth, stringY + 1, TextShadowColor);
 
-                                    OverlayFont.DrawString(null, message.Sender + ": ", TextStartWidth - 1, TextStartHeight + FontHeight * y, drawColor);
+                                    OverlayFont.DrawString(null, message.Sender + ": ", TextStartWidth - 1, stringY, drawColor);
                                 }
                                 y--;
                             }
@@ -400,7 +420,14 @@ namespace DotaDXInject
         private void LoadFont(Device device)
         {
             // Calculate the height of the font.
-            float fontHeight = 12.0f * 6 / MessagesShown / 1080f * PreviousHeight;
+            float fontHeight;
+
+            if (AutoMessageHeight)
+                // Scale the font if in automatic mode.
+                fontHeight = 12.0f * 6 / MessagesShown / 1080f * PreviousHeight;
+            else
+                // Use the set height if automatic mode is off.
+                fontHeight = MessageHeight;
 
             // Used to space the lines.
             FontHeight = (int) (fontHeight * 1.6);
@@ -430,6 +457,7 @@ namespace DotaDXInject
             // *Everything* in the Dota 2 interface is sized by percentage, woho!
             AvailableTextWidth = (int)(515f / 1920f * width);
             TextStartHeight = (int)(640f / 1080f * height);
+            TextEndHeight = (int)(808f / 1080f * height);
             TextStartWidth = (int)(29f / 1920f * width);
         }
 

Dota2ChatInterface/Windows/Settings.xaml
@@ -7,7 +7,7 @@
             <RowDefinition Height="35"/>
             <RowDefinition Height="90"/>
             <RowDefinition Height="35"/>
-            <RowDefinition Height="131"/>
+            <RowDefinition Height="157"/>
             <RowDefinition Height="35"/>
             <RowDefinition Height="*"/>
         </Grid.RowDefinitions>
@@ -41,14 +41,17 @@
         <TextBox x:Name="MESSAGES_SHOWN" HorizontalAlignment="Left" Height="23" Margin="117,43,0,0" Grid.Row="3" TextWrapping="Wrap" Text="6" VerticalAlignment="Top" Width="110"/>
         <Label Content="Messages shown" HorizontalAlignment="Left" Margin="11,41,0,0" Grid.Row="3" VerticalAlignment="Top" Height="26" Width="101" Padding="5,5,0,5"/>
         <Label Content="(Will determine font size)" HorizontalAlignment="Left" Margin="232,41,0,0" Grid.Row="3" VerticalAlignment="Top" Foreground="#FFC5C5C5" Height="26"/>
-        <CheckBox x:Name="FADE_MESSAGES" Content="Fade messages" HorizontalAlignment="Left" Margin="15,72,0,0" Grid.Row="3" VerticalAlignment="Top" IsChecked="True"/>
-        <Label Content="Fade delay" HorizontalAlignment="Left" Margin="15,93,0,0" Grid.Row="3" VerticalAlignment="Top"/>
-        <TextBox x:Name="FADE_WAIT" HorizontalAlignment="Left" Height="23" Margin="87,95,0,0" Grid.Row="3" TextWrapping="Wrap" Text="20" VerticalAlignment="Top" Width="39"/>
-        <Label Content="Fade duration" HorizontalAlignment="Left" Margin="141,93,0,0" Grid.Row="3" VerticalAlignment="Top"/>
-        <TextBox x:Name="FADE_DURATION" HorizontalAlignment="Left" Height="23" Margin="231,95,0,0" Grid.Row="3" TextWrapping="Wrap" Text="2,5" VerticalAlignment="Top" Width="39"/>
-        <Label Content="(Seconds)" HorizontalAlignment="Left" Margin="275,93,0,0" Grid.Row="3" VerticalAlignment="Top" Foreground="#FFC5C5C5" Height="26"/>
+        <CheckBox x:Name="FADE_MESSAGES" Content="Fade messages" HorizontalAlignment="Left" Margin="15,103,0,0" Grid.Row="3" VerticalAlignment="Top" IsChecked="True"/>
+        <Label Content="Fade delay" HorizontalAlignment="Left" Margin="10,123,0,0" Grid.Row="3" VerticalAlignment="Top"/>
+        <TextBox x:Name="FADE_WAIT" HorizontalAlignment="Left" Height="23" Margin="87,126,0,0" Grid.Row="3" TextWrapping="Wrap" Text="20" VerticalAlignment="Top" Width="39"/>
+        <Label Content="Fade duration" HorizontalAlignment="Left" Margin="141,123,0,0" Grid.Row="3" VerticalAlignment="Top"/>
+        <TextBox x:Name="FADE_DURATION" HorizontalAlignment="Left" Height="23" Margin="231,126,0,0" Grid.Row="3" TextWrapping="Wrap" Text="2,5" VerticalAlignment="Top" Width="39"/>
+        <Label Content="(Seconds)" HorizontalAlignment="Left" Margin="275,123,0,0" Grid.Row="3" VerticalAlignment="Top" Foreground="#FFC5C5C5" Height="26"/>
         <Button x:Name="CancelButton" Content="Cancel" HorizontalAlignment="Right" Margin="0,0,10,10" Grid.Row="5" VerticalAlignment="Bottom" Width="75" Height="28"/>
         <Button x:Name="SaveButton" Content="Save" HorizontalAlignment="Right" Margin="0,0,91,10" Grid.Row="5" VerticalAlignment="Bottom" Width="75" Height="28"/>
         <Button x:Name="DefaultFontButton" Content="Default" HorizontalAlignment="Left" Margin="236,12,0,0" Grid.Row="3" VerticalAlignment="Top" Width="75" Height="23"/>
+        <TextBox x:Name="MESSAGE_HEIGHT" HorizontalAlignment="Left" Height="23" Margin="116,74,0,0" Grid.Row="3" TextWrapping="Wrap" Text="12" VerticalAlignment="Top" Width="110" IsEnabled="False"/>
+        <Label Content="Message height" HorizontalAlignment="Left" Margin="10,72,0,0" Grid.Row="3" VerticalAlignment="Top" Height="26" Width="101" Padding="5,5,0,5"/>
+        <CheckBox x:Name="AUTO_MESSAGE_HEIGHT" Content="Auto" HorizontalAlignment="Left" Margin="236,77,0,0" Grid.Row="3" VerticalAlignment="Top" IsChecked="True"/>
     </Grid>
 </Window>

Dota2ChatInterface/Windows/Settings.xaml.cs
@@ -60,6 +60,10 @@ namespace Dota2ChatInterface
             // Register easter egg (spoiler?)
             FONT_NAME.TextChanged += FontName_TextChanged;
 
+            // Register event for AutoMessageHeight change.
+            AUTO_MESSAGE_HEIGHT.Checked += AUTO_MESSAGE_HEIGHT_Checked;
+            AUTO_MESSAGE_HEIGHT.Unchecked += AUTO_MESSAGE_HEIGHT_Checked;
+
             // Load settings.
             LoadedSettings = SettingsHandler.CloneInstance();
 
@@ -74,6 +78,8 @@ namespace Dota2ChatInterface
             FADE_MESSAGES.IsChecked = LoadedSettings.FadeMessages;
             FADE_WAIT.Text = LoadedSettings.FadeWait.ToString();
             FADE_DURATION.Text = LoadedSettings.FadeDuration.ToString();
+            AUTO_MESSAGE_HEIGHT.IsChecked = LoadedSettings.AutoMessageHeight;
+            MESSAGE_HEIGHT.Text = LoadedSettings.MessageHeight.ToString();
         }
 
         // Called when SaveButton is clicked.
@@ -115,6 +121,13 @@ namespace Dota2ChatInterface
             DefaultFontButton.Visibility = comicSans ? Visibility.Hidden : Visibility.Visible;
         }
 
+        // Called when the AutoMessageHeight property is changed.
+        private void AUTO_MESSAGE_HEIGHT_Checked(object sender, EventArgs args)
+        {
+            // Disable the MessageHeight textfield if enabled.
+            MESSAGE_HEIGHT.IsEnabled = !AUTO_MESSAGE_HEIGHT.IsChecked.Value;
+        }
+
         // Stores values in the SettingsHandler instance.
         private Boolean StoreTemporarily()
         {
@@ -128,6 +141,7 @@ namespace Dota2ChatInterface
             LoadedSettings.AddOnStartup = ADD_ON_STARTUP.IsChecked.Value;
             LoadedSettings.OutputAll = OUTPUT_ALL.IsChecked.Value;
             LoadedSettings.FadeMessages = FADE_MESSAGES.IsChecked.Value;
+            LoadedSettings.AutoMessageHeight = AUTO_MESSAGE_HEIGHT.IsChecked.Value;
 
             try
             {
@@ -192,6 +206,19 @@ namespace Dota2ChatInterface
                 MessageBox.Show("The field 'Fade duration' does not contain a valid number.", "A setting failed to save");
             }
 
+            try
+            {
+                LoadedSettings.MessageHeight = Int16.Parse(MESSAGE_HEIGHT.Text.Trim());
+            }
+            catch
+            {
+                // Number parsing failed.
+                parsingError = true;
+
+                // Alert the user.
+                MessageBox.Show("The field 'Message height' does not contain a valid integer.", "A setting failed to save");
+            }
+
             return parsingError;
         }
     }
@@ -212,6 +239,8 @@ namespace Dota2ChatInterface
         public Boolean FadeMessages = true;
         public double FadeWait = 20.0;
         public double FadeDuration = 2.5;
+        public Boolean AutoMessageHeight = true;
+        public Int16 MessageHeight = 12;
         
         // Indicates whether the instance can be saved or not. Only the original instance can be saved.
         private Boolean CanSave = true;
@@ -265,6 +294,8 @@ namespace Dota2ChatInterface
                 FadeMessages = reader.ReadBoolean();
                 FadeWait = reader.ReadDouble();
                 FadeDuration = reader.ReadDouble();
+                AutoMessageHeight = reader.ReadBoolean();
+                MessageHeight = reader.ReadInt16();
             }
             catch (Exception)
             {
@@ -309,6 +340,8 @@ namespace Dota2ChatInterface
                 writer.Write(FadeMessages);
                 writer.Write(FadeWait);
                 writer.Write(FadeDuration);
+                writer.Write(AutoMessageHeight);
+                writer.Write(MessageHeight);
             }
             catch (Exception)
             {
@@ -335,6 +368,8 @@ namespace Dota2ChatInterface
             this.FadeMessages = settingsHandler.FadeMessages;
             this.FadeWait = settingsHandler.FadeWait;
             this.FadeDuration = settingsHandler.FadeDuration;
+            this.AutoMessageHeight = settingsHandler.AutoMessageHeight;
+            this.MessageHeight = settingsHandler.MessageHeight;
         }
 
         // Returns a copy of the static instance. This copy can not save itself to disk.
@@ -353,6 +388,8 @@ namespace Dota2ChatInterface
             handler.FadeMessages = this.FadeMessages;
             handler.FadeWait = this.FadeWait;
             handler.FadeDuration = this.FadeDuration;
+            handler.AutoMessageHeight = this.AutoMessageHeight;
+            handler.MessageHeight = this.MessageHeight;
 
             return handler;
         }
@@ -366,6 +403,8 @@ namespace Dota2ChatInterface
             InjectionHelper.SendSetting("FadeMessages", this.FadeMessages);
             InjectionHelper.SendSetting("FadeWait", this.FadeWait);
             InjectionHelper.SendSetting("FadeDuration", this.FadeDuration);
+            InjectionHelper.SendSetting("AutoMessageHeight", this.AutoMessageHeight);
+            InjectionHelper.SendSetting("MessageHeight", this.MessageHeight);
         }
 
         // Returns the static instance or creates one if none is available.

commit 7512a8d90387230143d688857da9613a262f29c6

Adjusted the overlay height
Fixed a bug introcuded in 3d4a956057e4278db3eaf1bf6869344c40a42f1a which
caused the overlay to be displayed on top of the minimap.

DotaDXInject/DirectX/DXHookD3D9.cs
@@ -457,7 +457,7 @@ namespace DotaDXInject
             // *Everything* in the Dota 2 interface is sized by percentage, woho!
             AvailableTextWidth = (int)(515f / 1920f * width);
             TextStartHeight = (int)(640f / 1080f * height);
-            TextEndHeight = (int)(808f / 1080f * height);
+            TextEndHeight = (int)(775f / 1080f * height);
             TextStartWidth = (int)(29f / 1920f * width);
         }
 